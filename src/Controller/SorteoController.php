624d8e44ac9f85820c5017a89f224cc97685f6e2
Alfonso Jiménez <yo@alfonsojimenez.net>
2020-11-14 09:18:05 +0100

    2.4

--- src/Controller/SorteoController.php
+++ src/Controller/SorteoController.php
@@ -6,10 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SorteoController extends AbstractController
 {
-    public function numero()
+    public function numero($maximo)
     {
-        $numero = random_int(0, 100);
+        $numero = random_int(0, $maximo);
 
-        return $this->render('sorteo/numero.html.twig', ['numero' => $numero]);
+        return $this->render('sorteo/numero.html.twig', [
+            'numero' => $numero,
+        ]);
     }
 }
